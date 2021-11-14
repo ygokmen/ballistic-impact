6dea49eddab95c4b02841799795fcc03b48296fd
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-11-14 20:06:04 +0100

    Added Missing Semicolons
    Added Missing Semicolons

--- addons/goko_BI/cfgFunctions.hpp
+++ addons/goko_BI/cfgFunctions.hpp
@@ -13,32 +13,32 @@ class CfgFunctions
 			class BallisticImpactMain
 			{
 				file = "goko_BI\functions\fn_gokoBI_main.sqf";
-				preInit = 1
+				preInit = 1;
 			};
 			class particlefxBlood
 			{
 				file = "goko_BI\functions\fn_particleEffectsBlood.sqf";
-				preInit = 1
+				preInit = 1;
 			};
 			class particlefxHeadgear
 			{
 				file = "goko_BI\functions\fn_particleEffectsHeadgear.sqf";
-				preInit = 1
+				preInit = 1;
 			};
 			class particlefxHMD
 			{
 				file = "goko_BI\functions\fn_particleEffectsHMD.sqf";
-				preInit = 1
+				preInit = 1;
 			};
 			class particlefxOther
 			{
 				file = "goko_BI\functions\fn_particleEffectsOther.sqf";
-				preInit = 1
+				preInit = 1;
 			};
 			class gokobi_optionalcba
 			{
 				file = "goko_BI\functions\gokobi_options.sqf";
-				preInit = 1
+				preInit = 1;
 			};
 		};
 	};
